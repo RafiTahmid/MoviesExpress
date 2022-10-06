9f8444d2d93a9eb109a5375a98888398d821437b
RafiTahmid <rafi.tahmid019@gmail.com>
2022-10-06 13:03:54 +0600

    fetched data and loaded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 
+import { useEffect } from 'react';
 import './App.css';
+import SearchIcon from './search.svg';
+// e9ca62f3
 
-function App() {
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=e9ca62f3'
+
+const App = () => {
+
+  const searchMovies = async (title) =>{
+    const res = await fetch(`${API_URL}&s=${title}`)
+    const data = await res.json()
+    console.log(data.Search)
+  }
+
+  useEffect( () => {
+    searchMovies('Spiderman')
+  },[]);
   return (
-    <div className="App">
-      
+    <div className="app">
+      <h1>MoviesExpress</h1>
+
+      <div className="search">
+        <input
+        placeholder='Search for Movies'
+        value='Superman' 
+        onChange={ () => {}}/>
+
+        <img src={SearchIcon} alt="" onClick={ () => {}}/>
+      </div>
     </div>
+    
   );
 }
 
