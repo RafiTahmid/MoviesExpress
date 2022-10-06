8b68dacdf75e1d4fc569c61b7086692289ce0061
RafiTahmid <rafi.tahmid019@gmail.com>
2022-10-06 13:31:14 +0600

    movieCard added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@
 import { useEffect } from 'react';
 import './App.css';
 import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
 // e9ca62f3
 
 const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=e9ca62f3'
+
+const movie1 = {
+  "Title": "Spiderman in Cannes",
+  "Year": "2016",
+  "imdbID": "tt5978586",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg"
+}
 
 const App = () => {
 
@@ -28,6 +37,10 @@ const App = () => {
         onChange={ () => {}}/>
 
         <img src={SearchIcon} alt="" onClick={ () => {}}/>
+      </div>
+
+      <div className="container">
+        <MovieCard></MovieCard>
       </div>
     </div>
     
